05bee485945a1f8c81ee1bf738728cffbcc7aa3b
임수빈 <hange253@gmail.com>
2023-05-24 20:17:22 +0900

    [CREATE] login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,21 @@
+import Background from "@/components/login/background";
+import { theme } from "@/styles/theme";
+import styled from "@emotion/styled";
+
 export default function Login() {
   return (
-    <>
-      <h1>login</h1>
-    </>
+    <Container>
+      <Background />
+    </Container>
   );
 }
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${theme.backgroundColor};
+  overflow-y: hidden;
+`;
